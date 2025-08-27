147f2ef2523f2f0308e8fd7ba1a7938bf20d404b
Deniskish <kisheryt2007@gmail.com>
2025-08-27 23:06:52 +0300

    replace connect

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <TonConnectUIProvider manifestUrl="/tonconnect-manifest.json">
+    <TonConnectUIProvider manifestUrl="https://checkers-tel-app-so51.vercel.app/tonconnect-manifest.json">
       <App />
     </TonConnectUIProvider>
   </React.StrictMode>
